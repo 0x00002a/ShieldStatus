6460682b28cca27fd7ef6d7756c6917a5462f5af
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2021-09-20 22:43:50 +0100

    Add project files.

---
+++ data/scripts/ShieldStatus/ShieldStatus.cs
@@ -0,0 +1,141 @@
+﻿using Sandbox.Game.GameSystems.TextSurfaceScripts;
+using Sandbox.ModAPI;
+using System;
+using VRage.Game.ModAPI;
+using VRageMath;
+
+using DefenseShields;
+using VRage.Game.GUI.TextPanel;
+using System.Collections.Generic;
+using VRage.ModAPI;
+
+namespace Natomic.ShieldStatus
+{
+    [MyTextSurfaceScript("ShieldStatus", "Shield Status")]
+    public class ShieldStatus : MyTextSurfaceScriptBase
+    {
+        public override ScriptUpdate NeedsUpdate => ScriptUpdate.Update10;
+
+        internal readonly ShieldApi ds_api_ = new ShieldApi();
+        internal Exception err_ = null;
+        internal readonly List<MySprite> sprites_ = new List<MySprite>();
+        internal readonly SpriteBuilder builder_ = new SpriteBuilder();
+        internal Vector2 progress_sprite_size_;
+        internal IMyTerminalBlock shield_block_;
+
+
+        public ShieldStatus(IMyTextSurface surface, IMyCubeBlock block, Vector2 size) : base(surface, block, size)
+        {
+            if (!ds_api_.Load())
+            {
+                err_ = new Exception("failed to load defense shields");
+                return;
+            }
+            builder_.Surface = surface;
+
+            builder_.Viewport = new RectangleF(new Vector2(0, (surface.TextureSize.Y - surface.SurfaceSize.Y) / 2f), surface.SurfaceSize);
+            builder_.Scale = 1;
+            progress_sprite_size_ = new Vector2(surface.SurfaceSize.X / 4 * 3, SpriteBuilder.NEWLINE_HEIGHT_BASE / 4 * 3);
+            shield_block_ = ds_api_.MatchEntToShieldFast((IMyEntity)block.CubeGrid, false);
+            if (shield_block_ == null)
+            {
+                err_ = new Exception("failed to find shield");
+                return;
+            }
+        }
+
+        private float FindCurrShieldHP()
+        {
+            return ds_api_.GetCharge(shield_block_);
+        }
+        public override void Run()
+        {
+            Draw();
+        }
+        private static Color ColourForPercent(float percent)
+        {
+           if (percent >= 60)
+            {
+                return Color.Green;
+            } else if (percent >= 30)
+            {
+                return Color.Orange;
+            } else if (percent >= 10)
+            {
+                return Color.DarkOrange;
+            } else
+            {
+                return Color.Red;
+            }
+        }
+        internal Vector2 CenterOfSurfaceX()
+        {
+            return new Vector2(Surface.SurfaceSize.X / 2f, 0);
+        }
+        private void DrawProgressBar()
+        {
+            var shield_status = FindCurrShieldHP();
+            var shield_max = ds_api_.GetMaxCharge(shield_block_);
+            var status_color = ColourForPercent(shield_status);
+            using (var ident = builder_.WithIndent((int)(CenterOfSurfaceX().X - progress_sprite_size_.X / 2f)))
+            {
+                builder_.MakeProgressBar(sprites_, progress_sprite_size_, Color.White, status_color, shield_status, shield_max);
+            }
+            builder_.AddNewline();
+            sprites_.Add(builder_.MakeText(
+                $"{(100 * shield_status / shield_max)} %",
+                alignment: TextAlignment.CENTER,
+                color: status_color,
+                offset: CenterOfSurfaceX()
+                ));
+        }
+        public override void Dispose()
+        {
+            ds_api_?.Unload();
+        }
+        private void DrawPowerUse()
+        {
+            var pow = ds_api_.GetPowerUsed(shield_block_);
+            sprites_.Add(builder_.MakeText($"Power use: {pow}Mw", alignment: TextAlignment.RIGHT, offset: new Vector2(Surface.SurfaceSize.X, 0)));
+        }
+        private void DrawHeat()
+        {
+            var heat = ds_api_.GetShieldHeat(shield_block_);
+            sprites_.Add(builder_.MakeText($"Heat: {heat}"));
+        }
+        private void DrawStatus()
+        {
+            var status = ds_api_.ShieldStatus(shield_block_);
+            sprites_.Add(builder_.MakeText($"{status}", alignment: TextAlignment.CENTER, offset: CenterOfSurfaceX() ));
+        }
+        private void Draw()
+        {
+            sprites_.Clear();
+            builder_.CurrPos = new Vector2(0, 0);
+            if (err_ != null)
+            {
+                DrawErr();
+            }
+            else
+            {
+                DrawShieldInfo();
+            }
+            using (var frame = Surface.DrawFrame())
+            {
+                frame.AddRange(sprites_);
+            }
+        }
+        private void DrawErr()
+        {
+            sprites_.Add(builder_.MakeText($"error during run: {err_.Message}", TextAlignment.CENTER, Color.Red));
+        }
+        private void DrawShieldInfo()
+        {
+            DrawStatus();
+            builder_.AddNewline();
+            DrawProgressBar();
+            builder_.AddNewline();
+        }
+
+    }
+}
---
+++ data/scripts/ShieldStatus/SpriteBuilder.cs
@@ -0,0 +1,172 @@
+﻿using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VRage.Game.GUI.TextPanel;
+using VRageMath;
+
+namespace Natomic.ShieldStatus
+{
+    public class SpriteBuilder
+        {
+            public struct IndentProxy: IDisposable {
+                internal SpriteBuilder parent_;
+                internal int by_;
+                internal IndentProxy(SpriteBuilder parent, int by)
+                {
+                    parent_ = parent;
+                    by_ = by;
+                    parent.CurrPos.X += by;
+                }
+
+                public void Dispose()
+                {
+                    parent_.CurrPos.X -= by_;
+                }
+            }
+            public struct BoxedProxy {
+                internal SpriteBuilder parent_;
+                internal Vector2 origin_;
+                public BoxedProxy(SpriteBuilder parent)
+                {
+                    parent_ = parent;
+                    origin_ = parent.CurrPos;
+                }
+
+                public void Make(ref MySpriteDrawFrame to, int width, int line_width)
+                {
+                    var size = new RectangleF(origin_, new Vector2( width, parent_.CurrPos.Y - origin_.Y));
+
+                    to.Add(parent_.MakeRect( // Right
+                        start: new Vector2(origin_.X + width - line_width, origin_.Y),
+                        end: new Vector2(origin_.X + width, parent_.CurrPos.Y)
+                        )); 
+                }
+            }
+            
+
+            public float Scale;
+            public RectangleF Viewport;
+            public Vector2 CurrPos;
+            public IMyTextSurface Surface;
+
+
+            internal Vector2 NewlineCenterOffset => new Vector2(0, NewlineHeight / 4);
+            internal StringBuilder str_cache_;
+
+            public float NewlineHeight => NEWLINE_HEIGHT_BASE * Scale;
+
+            public const float NEWLINE_HEIGHT_BASE = 37f;
+
+            public Vector2 TextSizePx(string txt, IMyTextSurface reference = null,  string font_id = "White")
+            {
+                reference = reference ?? Surface;
+                if (str_cache_ == null)
+                {
+                    str_cache_ = new StringBuilder();
+                } else
+                {
+                    str_cache_.Clear();
+                }
+
+                str_cache_.Append(txt);
+                var size = reference.MeasureStringInPixels(str_cache_, font_id, Scale);
+                str_cache_.Clear();
+                return size;
+
+            }
+
+            public IndentProxy WithIndent(int by)
+            {
+                return new IndentProxy(this, by);
+            }
+
+            public void AddIndent(int by)
+            {
+                CurrPos.X += by;
+            }
+            public void AddNewline()
+            {
+                CurrPos.Y += NewlineHeight;
+            }
+            public MySprite MakeRect(Vector2 start, Vector2 end, Color? fg = null)
+            {
+                var rect = new RectangleF(start, end - start);
+                var colour = fg ?? Color.White;
+                return new MySprite
+                {
+                    Type = SpriteType.TEXTURE,
+                    Data = "SquareSimple",
+                    Color = colour,
+                    Size = rect.Size,
+                    Position = rect.Center,
+                    Alignment = TextAlignment.CENTER,
+
+                };
+            }
+            public MySprite MakeBulletPt(Color? fg = null)
+            {
+                var color = fg ?? Color.White;
+                var bounds = new RectangleF(CurrPos, new Vector2(NewlineHeight / 3, NewlineHeight / 3));
+                return new MySprite
+                {
+                    Data = "Circle",
+                    Type = SpriteType.TEXTURE,
+                    Color = color,
+                    Alignment = TextAlignment.CENTER,
+                    Position = bounds.Center + NewlineCenterOffset,
+                    Size = bounds.Size,
+                };
+            }
+
+            public void MakeProgressBar(ICollection<MySprite> to, Vector2 size, Color bg, Color fg, double curr, double total)
+            {
+                var padding = new Vector2(2, 2);
+                size *= Scale;
+
+                var bg_rect = new RectangleF(CurrPos, size);
+
+                var sprite = new MySprite
+                {
+                    Type = SpriteType.TEXTURE,
+                    Data = "SquareHollow",
+                    Position = bg_rect.Center + NewlineCenterOffset,
+                    Color = bg,
+                    Alignment = TextAlignment.CENTER,
+                    Size = bg_rect.Size,
+                };
+                to.Add(sprite);
+
+                var fg_rect = new RectangleF(CurrPos + padding / 2, new Vector2((float)(curr * (size.X / total)), size.Y) - padding);
+
+                sprite = new MySprite
+                {
+                    Type = SpriteType.TEXTURE,
+                    Data = "SquareSimple",
+                    Position = fg_rect.Center + NewlineCenterOffset,
+                    Color = fg,
+                    Alignment = TextAlignment.CENTER,
+                    Size = fg_rect.Size,
+                };
+                to.Add(sprite);
+                
+
+            }
+
+            public MySprite MakeText(string txt, TextAlignment alignment = TextAlignment.LEFT, Color? color = null, string font_id = "White", Vector2? offset = null)
+            {
+                var roffset = offset ?? Vector2.Zero;
+                return new MySprite
+                {
+                    Type = SpriteType.TEXT,
+                    Data = txt,
+                    Alignment = alignment,
+                    RotationOrScale = Scale,
+                    FontId = font_id,
+                    Color = color ?? Color.White,
+                    Position = CurrPos + roffset,
+                };
+            }
+
+        }
+    }
